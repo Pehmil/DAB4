f9156806ebaae34a806bbdd821a5c71a52ea8d53
Pehmil <pehmil@gmail.com>
2018-05-16 12:43:35 +0200

    Added most classes, not implemented yet

---
+++ DAB4/Prosumers/Private.cs
@@ -0,0 +1,16 @@
+﻿using DAB4.Interfaces;
+
+namespace DAB4.Prosumers
+{
+    class Private : IProsumer
+    {
+        public string Id { get; set; }
+        public int ProducedkW { get; set; }
+        public int ConsumedkW { get; set; }
+
+        public void CalcPower()
+        {
+
+        }
+    }
+}
---
+++ DAB4/Village/VillageSmartGrid.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAB4.Village
+{
+    class VillageSmartGrid
+    {
+        private List<Smartmeter> _vsgList = new List<Smartmeter>();
+
+    }
+}
