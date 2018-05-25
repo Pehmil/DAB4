0dbb011c471135500aecd9c6687f2272c99eeba9
LisbethWL <contact@spacegap.dk>
2018-05-25 12:11:38 +0200

    smartmeter public prosumer

---
+++ ProsumerDB/Models/Prosumer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using DAB4.Village;
+
+
+namespace ProsumerDB.Models
+{
+    public class Prosumer
+    {
+        public int Id { get; set; }
+
+        public int ProducedkW { get; set; }
+        public int ConsumedkW { get; }
+
+        [Required]
+        public string Type { get; set; }
+    }
+}
